44fe28e4a797ae2ac517144b8b43120bc70fe805
TotochuSSchool <maxime.catho@gmail.com>
2025-04-17 14:32:12 +0200

    Repair and andd csca

--- app/src/main/java/com/example/nfc/MainActivity.kt
+++ app/src/main/java/com/example/nfc/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.nfc.ui.theme.NFCTheme
